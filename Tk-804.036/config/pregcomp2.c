cc2432cfa1395cd099b112f413bce51cc16b2ec6
Bernhard Rosenkränzer <bero@lindev.ch>
2026-07-27 17:38:06 +0200

    Remove accidentally committed upstream source tree
    Sources belong in file-store via .abf.yml, not the git repo.

--- Tk-804.036/config/pregcomp2.c
+++
@@ -1,8 +0,0 @@
-#include <EXTERN.h>
-#include <perl.h>
-#include <XSUB.h>
-
-int main() {
-    SV* sv = newSViv(0);
-    void* rx = pregcomp(sv, 0);
-}
--- Tk-804.036/config/signedchar.c
+++
@@ -1,5 +0,0 @@
-int main(void)
-{
- signed char x = 'a';
- return (x - 'a');
-}
